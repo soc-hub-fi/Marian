e8c9a57daa2b98e156f68d2457c30bf0db776c52
Tom Szymkowiak <phys2928@ox.ac.uk>
2026-01-21 12:28:39 +0200

    add initial slang config

---
+++ tools/slang-server.f
@@ -0,0 +1,8 @@
+# Includes
++incdir+ips/pulp_apb/include/
++incdir+ips/bow_common_ips/ips/axi/include/
++incdir+ips/bow_common_ips/ips/pulp-common-cells/include/
++incdir+ips/pulp_ara/hardware/include/
++incdir+ips/pulp_reg_if/include/
+# Defines
++define+L2_NUM_ROWS=16384
